e1724581b06af20e3ec43eb822697786900b4c7a
Alexander Dobrynin <info@alex-dobrynin.com>
2022-06-21 15:21:54 +0200

    duda files now contain a version scheme. an appropriate parser will be chosen (#3)

--- app/parsing/ModuleCompendiumParser.scala
+++ app/parsing/ModuleCompendiumParser.scala
@@ -3,13 +3,12 @@ package parsing
 import parser.Parser
 import parser.Parser.newline
 import parsing.content.ContentParser.contentParser
-import parsing.metadata.MetadataParser.metadataV1Parser
+import parsing.metadata.MetadataParser.metadataParser
 import parsing.types.ModuleCompendium
 
 object ModuleCompendiumParser {
-
   val moduleCompendiumParser: Parser[ModuleCompendium] =
-    metadataV1Parser
+    metadataParser
       .skip(newline.many())
       .zip(contentParser)
       .map(a => ModuleCompendium(a._1, a._2._1, a._2._2))
