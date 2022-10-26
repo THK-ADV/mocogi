102e91ae2d3f2ac90c4cddebb416377e96ab9b22
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2022-10-26 17:28:21 +0200

    assessment method controller stack

--- test/parsing/base/AssessmentMethodFileParserSpec.scala
+++ test/parsing/base/AssessmentMethodFileParserSpec.scala
@@ -1,21 +1,15 @@
 package parsing.base
 
 import basedata.AssessmentMethod
-import helper.FakeApplication
 import org.scalatest.EitherValues
 import org.scalatest.wordspec.AnyWordSpec
-import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import parsing.base.AssessmentMethodFileParser.fileParser
 import parsing.{ParserSpecHelper, withFile0}
 
 class AssessmentMethodFileParserSpec
     extends AnyWordSpec
     with ParserSpecHelper
-    with EitherValues
-    with GuiceOneAppPerSuite
-    with FakeApplication {
-
-  val parser =
-    app.injector.instanceOf(classOf[AssessmentMethodFileParser]).fileParser
+    with EitherValues {
 
   "A Assessment Method File Parser" should {
     "parse a single assessment method" in {
@@ -23,7 +17,8 @@ class AssessmentMethodFileParserSpec
         """written-exam:
           |  de_label: Klausurarbeiten
           |  en_label: written exam""".stripMargin
-      val (res, rest) = parser.parse(input)
+
+      val (res, rest) = fileParser.parse(input)
       assert(
         res.value == List(
           AssessmentMethod("written-exam", "Klausurarbeiten", "written exam")
@@ -49,7 +44,7 @@ class AssessmentMethodFileParserSpec
           |practical-report:
           |  de_label: Praktikumsbericht
           |  en_label: labwork report""".stripMargin
-      val (res, rest) = parser.parse(input)
+      val (res, rest) = fileParser.parse(input)
       assert(
         res.value == List(
           AssessmentMethod("project", "Projektarbeit", "project"),
@@ -72,37 +67,9 @@ class AssessmentMethodFileParserSpec
     "parse all assessment methods in assessment.yaml" in {
       val (res, rest) =
         withFile0("test/parsing/res/assessment.yaml")(
-          parser.parse
+          fileParser.parse
         )
-      assert(
-        res.value == List(
-          AssessmentMethod("written-exam", "Klausurarbeiten", "--"),
-          AssessmentMethod(
-            "written-exam-answer-choice-method",
-            "Schriftliche Prüfungen im Antwortwahlverfahren",
-            "--"
-          ),
-          AssessmentMethod("oral-exams", "Mündliche Prüfungen", "--"),
-          AssessmentMethod("presentation", "Präsentation", "--"),
-          AssessmentMethod("home-assignment", "Hausarbeit", "--"),
-          AssessmentMethod("project", "Projektarbeit", "--"),
-          AssessmentMethod(
-            "project-documentation",
-            "Projektdokumentation",
-            "--"
-          ),
-          AssessmentMethod("portfolio", "Lernportfolio", "--"),
-          AssessmentMethod("practical-report", "Praktikumsbericht", "--"),
-          AssessmentMethod(
-            "practical-semester-report",
-            "Praxissemesterbericht",
-            "--"
-          ),
-          AssessmentMethod("practical", "Praktikum", "--"),
-          AssessmentMethod("test", "Schriftlicher Test", "--"),
-          AssessmentMethod("thesis", "Schriftliche Ausarbeitung", "--")
-        )
-      )
+      assert(res.value.size == 22)
       assert(rest.isEmpty)
     }
   }
