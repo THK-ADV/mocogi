725584b363d3c74348e6ba6e1108ad8c85ee687a
Alexander Dobrynin <info@alex-dobrynin.com>
2022-10-31 15:59:21 +0100

    replaces StudyProgramPreview with String

--- app/controllers/FocusAreaController.scala
+++ app/controllers/FocusAreaController.scala
@@ -4,6 +4,7 @@ import basedata.FocusArea
 import controllers.json.FocusAreaFormat
 import play.api.libs.json.Writes
 import play.api.mvc.{AbstractController, ControllerComponents}
+import service.YamlService
 
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.ExecutionContext
@@ -15,7 +16,13 @@ final class FocusAreaController @Inject() (
 ) extends AbstractController(cc)
     with FocusAreaFormat
     with YamlController[FocusArea, FocusArea] {
-  override val service = ???
+  override val service = new YamlService[FocusArea, FocusArea] {
+    override def repo = ???
+
+    override def parser = ???
+
+    override def toInput(output: FocusArea) = ???
+  }
   override implicit val writesOut: Writes[FocusArea] = focusAreaFormat
   override implicit val writesIn: Writes[FocusArea] = focusAreaFormat
 }
--- app/controllers/json/FocusAreaFormat.scala
+++ app/controllers/json/FocusAreaFormat.scala
@@ -1,12 +1,9 @@
 package controllers.json
 
-import basedata.{FocusArea, StudyProgramPreview}
+import basedata.FocusArea
 import play.api.libs.json.{Format, Json}
 
 trait FocusAreaFormat {
-
-  implicit val studyProgramPreview: Format[StudyProgramPreview] =
-    Json.format[StudyProgramPreview]
 
   implicit val focusAreaFormat: Format[FocusArea] =
     Json.format[FocusArea]
