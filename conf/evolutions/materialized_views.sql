5b5ae1ac95ada92074b800404da877e829275e82
Alexander Dobrynin <info@alex-dobrynin.com>
2023-07-28 13:45:26 +0200

    adds more fields to projection of study-program-atomic (#67)

--- conf/evolutions/materialized_views.sql
+++ conf/evolutions/materialized_views.sql
@@ -1,10 +1,12 @@
 -- study_program_atomic
 create materialized view study_program_atomic as
 select study_program.de_label as sp_label,
+       study_program.abbrev   as sp_abbrev,
        grade.de_label         as grade_label,
        po.abbrev              as po_abbrev,
        po.version             as po_version,
-       specialization.label   as spec_label
+       specialization.label   as spec_label,
+       specialization.abbrev  as spec_abbrev
 from study_program
          join grade on study_program.grade = grade.abbrev
          join po on po.study_program = study_program.abbrev
