e6f8547e618876058c331ca9836fdb61a02afe3c
Alexander Dobrynin <info@alex-dobrynin.com>
2023-07-28 13:42:32 +0200

    adds more fields to projection of study-program-atomic

--- app/controllers/formats/StudyProgramAtomicFormat.scala
+++ app/controllers/formats/StudyProgramAtomicFormat.scala
@@ -1,9 +1,12 @@
 package controllers.formats
 
-import database.view.StudyProgramAtomic
+import database.view.{SpecializationShort, StudyProgramAtomic}
 import play.api.libs.json.{Format, Json}
 
 trait StudyProgramAtomicFormat extends JsonNullWritable {
+  implicit val specializationShortFmt: Format[SpecializationShort] =
+    Json.format[SpecializationShort]
+
   implicit val studyProgramAtomicFmt: Format[StudyProgramAtomic] =
     Json.format[StudyProgramAtomic]
 }
