d14fde6b058f5f3af89e541d1b7bfbf1664860e2
Alexander Dobrynin <info@alex-dobrynin.com>
2023-11-13 10:45:30 +0100

    localized ModuleUpdatePermissionType #90

--- app/models/ModuleUpdatePermissionType.scala
+++ app/models/ModuleUpdatePermissionType.scala
@@ -1,18 +1,25 @@
 package models
 
-import play.api.libs.json.Writes
+import play.api.libs.json.{Json, Writes}
 
 sealed trait ModuleUpdatePermissionType {
   def id: String
+  def deLabel: String
+  def enLabel: String
   override def toString = id
 }
 
 object ModuleUpdatePermissionType {
   case object Inherited extends ModuleUpdatePermissionType {
-    override val id: String = "inherited"
+    override def id: String = "inherited"
+    override def deLabel: String = "Automatisch gesetzt"
+    override def enLabel: String = "Inherited"
   }
+
   case object Granted extends ModuleUpdatePermissionType {
-    override val id: String = "granted"
+    override def id: String = "granted"
+    override def deLabel: String = "Explizit vergeben"
+    override def enLabel: String = "Granted"
   }
 
   def apply(id: String): ModuleUpdatePermissionType =
@@ -22,5 +29,10 @@ object ModuleUpdatePermissionType {
     }
 
   implicit def writes: Writes[ModuleUpdatePermissionType] =
-    Writes.of[String].contramap(_.id)
+    o =>
+      Json.obj(
+        "id" -> o.id,
+        "deLabel" -> o.deLabel,
+        "enLabel" -> o.enLabel
+      )
 }
