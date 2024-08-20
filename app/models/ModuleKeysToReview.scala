ee48fdcf1bca1c8eb72825a97ae61891eb77018a
Alexander Dobrynin <info@alex-dobrynin.com>
2024-08-20 11:22:49 +0200

    removes SGL from keys to review (#139)

--- app/models/ModuleKeysToReview.scala
+++ app/models/ModuleKeysToReview.scala
@@ -1,9 +1,6 @@
 package models
 
-case class ModuleKeysToReview(sgl: Set[String], pav: Set[String]) {
-  def contains(key: String) = isSGLReview(key) || isPAVReview(key)
-
-  def isSGLReview(key: String) = sgl.contains(key)
-
+case class ModuleKeysToReview(pav: Set[String]) {
+  def contains(key: String) = isPAVReview(key)
   def isPAVReview(key: String) = pav.contains(key)
 }
