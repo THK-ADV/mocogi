1512d2607539088de99dd5476268273b6285c6f4
Alexander Dobrynin <info@alex-dobrynin.com>
2025-12-13 14:04:00 +0100

    feat: add API for uploading and retrieving module catalog intro files

--- app/printing/pandoc/WordLatexPrinter.scala
+++ app/printing/pandoc/WordLatexPrinter.scala
@@ -7,46 +7,30 @@ import java.nio.file.Paths
 import scala.sys.process.*
 import scala.util.Try
 
-import models.FullPoId
 import ops.FileOps.FileOps0
-import play.api.Logging
 
-final class WordLatexPrinter(cmd: String, outputFolder: String) extends Logging {
+final class WordLatexPrinter(cmd: String, outputFolder: String) {
 
-  def toLatex(wordPath: Path, fullPoId: FullPoId): Try[Path] =
+  def toLatex(wordPath: Path, po: String): Try[Path] =
     Try {
-      isWordFile(wordPath)
-      val dir              = createFolder(fullPoId.id)
-      val dest             = dir.resolve(s"${fullPoId.id}.tex")
-      val (code, out, err) = execCmd(dir, wordPath, dest)
-      if (out.nonEmpty) {
-        logger.info(out.toString())
+      val dir             = initFolder(po)
+      val dest            = dir.resolve(s"$po.tex")
+      val (exitCode, out) = execCmd(dir, wordPath, dest)
+      if exitCode == 0 then dest
+      else {
+        dir.deleteDirectory()
+        throw new Exception(s"failed to covert word to latex: ${out.toString()}")
       }
-      if (err.nonEmpty) {
-        logger.error(err.toString())
-      }
-      logger.info(s"printed $wordPath to tex file $dest. Return Code: $code")
-      dest
     }
 
-  private def isWordFile(path: Path): Unit =
-    Files.probeContentType(path) match
-      case "application/vnd.openxmlformats-officedocument.wordprocessingml.document" =>
-        ()
-      case null =>
-        throw new Exception("expected word file, but no content type was found")
-      case other =>
-        throw new Exception(s"expected word file, but was: $other")
-
-  private def execCmd(ctx: Path, src: Path, dest: Path) = {
+  private def execCmd(ctx: Path, src: Path, dest: Path): (Int, StringBuilder) = {
     val process = dest.toFile #< Process(cmd, ctx.toFile) #< src.toFile
     val out     = new StringBuilder()
-    val err     = new StringBuilder()
-    val pLogger = ProcessLogger(fout => out.append(s"$fout\n"), ferr => err.append(s"$ferr\n"))
-    (process ! pLogger, out, err)
+    val pLogger = ProcessLogger(s => out.append(s))
+    (process ! pLogger, out)
   }
 
-  private def createFolder(filename: String): Path = {
+  private def initFolder(filename: String): Path = {
     val dir = Paths.get(outputFolder).resolve(filename)
     if Files.isDirectory(dir) then dir.deleteDirectory()
     Files.createDirectory(dir)
