e82fdc5c54362359f7f62c7b9fe06bc2b7f0e470
Alexander Dobrynin <info@alex-dobrynin.com>
2026-03-13 17:42:00 +0100

    feat: semester break type in semseter plan

--- app/models/schedule/SemesterPlanType.scala
+++ app/models/schedule/SemesterPlanType.scala
@@ -3,6 +3,7 @@ package models.schedule
 enum SemesterPlanType(val id: String) {
   case Exam           extends SemesterPlanType("exam")
   case Lecture        extends SemesterPlanType("lecture")
+  case SemesterBreak  extends SemesterPlanType("semester_break")
   case Block          extends SemesterPlanType("block")
   case Project        extends SemesterPlanType("project")
   case ClosedBuilding extends SemesterPlanType("closed_building")
@@ -18,5 +19,6 @@ object SemesterPlanType {
       case "project"         => Project
       case "closed_building" => ClosedBuilding
       case "self_study"      => SelfStudy
+      case "semester_break"  => SemesterBreak
     }
 }
